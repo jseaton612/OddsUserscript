b81cd9b9ac7d1975e82c44597c2b7aef6fa300de
jseaton <jseaton612@gmail.com>
2020-06-24 13:11:01 -0400

    Update PokerOdds.js

--- PokerOdds.js
+++ PokerOdds.js
@@ -5,6 +5,7 @@
 // @description  Poker Odds
 // @author       somethingintheshadows
 // @match        https://www.zyngapoker.com/*
+// @updateURL    https://raw.githubusercontent.com/jseaton612/OddsUserscript/master/PokerOdds.js
 // @grant        none
 // @run-at document-start
 // ==/UserScript==
@@ -27,51 +28,51 @@
             ws = new OrigWebSocket();
         }
 
-        wsAddListener(ws, 'message', function(event) {
+        wsAddListener(ws, "message", function(event) {
             if (event.data.includes("dealHoles")) {
-                var holes = /%[\d-]+%([\d-]+)%([\d-]+)%([\d-]+)%([\d-]+)%([\d-]+)%[\d-]+%[\d-]+%[\d-]+%[\d-]+%/.exec(event.data)
-                console.log(holes.length);
+                var holes = /%[\d-]+%([\d-]+)%([\d-]+)%([\d-]+)%([\d-]+)%([\d-]+)%[\d-]+%[\d-]+%[\d-]+%[\d-]+%/.exec(event.data);
+                console.log(holes);
             } else if (event.data.includes("flop")) {
-                var cards = /\d%(\d+)%(\d)%(\d+)%(\d)%(\d+)%(\d)%/.exec(event.data)
+                var cards = /\d%(\d+)%(\d)%(\d+)%(\d)%(\d+)%(\d)%/.exec(event.data);
                 for (var i = 2; i < cards.length; i += 2) {
                     switch (parseInt(cards[i])) {
-                        case 0:
-                            cards[i-1] += "D";
-                            break;
-                        case 1:
-                            cards[i-1] += "H";
-                            break;
-                        case 2:
-                            cards[i-1] += "S";
-                            break;
-                       case 3:
-                            cards[i-1] += "C";
-                            break;
+                    case 0:
+                        cards[i-1] += "D";
+                        break;
+                    case 1:
+                        cards[i-1] += "H";
+                        break;
+                    case 2:
+                        cards[i-1] += "S";
+                        break;
+                    case 3:
+                        cards[i-1] += "C";
+                        break;
                     }
                 }
                 console.log(cards[1] + " " + cards[3] + " " + cards[5]);
             } else if (event.data.includes("street") || event.data.includes("river")) {
-                var card = /\d%(\d+)%(\d)%/.exec(event.data)
+                var card = /\d%(\d+)%(\d)%/.exec(event.data);
                 switch (parseInt(card[2])) {
-                    case 0:
-                        card[1] += "D";
-                        break;
-                    case 1:
-                        card[1] += "H";
-                        break;
-                    case 2:
-                        card[1] += "S";
-                        break;
-                   case 3:
-                        card[1] += "C";
-                        break;
+                case 0:
+                    card[1] += "D";
+                    break;
+                case 1:
+                    card[1] += "H";
+                    break;
+                case 2:
+                    card[1] += "S";
+                    break;
+                case 3:
+                    card[1] += "C";
+                    break;
                 }
                 console.log(card[1]);
             } else if (event.data.includes("sitsFilled")) {
                 console.log(/CDATA\[(\d)/.exec(event.data)[1] + " players");
             } else if (event.data.includes("fold")) {
                 console.log("Fold");
-            } else {console.log(event.data)}
+            } else {console.log(event.data);}
         });
         return ws;
     }.bind();
